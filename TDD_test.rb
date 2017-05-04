b2cf87963079da1f7d07ba73cd68cb1ba9c8806a
Unknown <stanshalomchin@yahoo.com>
2017-05-04 11:44:29 +0800

    Pass test 1
    1. assert_equal([], sort([])) , wrote passing production code

--- TDD_test.rb
+++ TDD_test.rb
@@ -2,8 +2,15 @@ require 'minitest/autorun'
 
 class RandomTest < Minitest::Test
 
-def test_sort
-  assert_equal([], sort([])) 
-end
+  def sort(array)
+    if array == []
+      return array
+    end
+  end
+
+  def test_sort
+  
+    assert_equal([], sort([]))
+  end
 
 end
