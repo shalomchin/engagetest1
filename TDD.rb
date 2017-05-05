2ef364c42d4dd6e1e750cc8b0389216bfc8b8d8e
Unknown <stanshalomchin@yahoo.com>
2017-05-05 09:13:32 +0800

    Add check if sorted function in production code
    3rd test still failed. Refactored so that checking if sort code works to still pass test 1 and test 2

--- TDD.rb
+++ TDD.rb
@@ -1,10 +1,38 @@
-class Random
-# def sort(array)
-#     if array == []
-#       puts array
-#     end
-# end
 
-
+def sort(array)
+  if array == [] || array == [1]
+    print array
+    return array
+  end
+  puts array
 end
 
+# def checksorted(array)
+#   array.each_with_index do |v, i|
+#     if array[i] <= array[i+1]-1
+#       print "it works"
+#       return array
+
+#     else
+#       #sort correct order
+#     end
+#   end
+# end
+
+def checksorted(array)
+  i = 0
+  until i > array.length-2 do
+    # print array[i]
+    if array[i] > array[i+1] then
+    
+      print array
+      
+    else
+      break
+    end
+    i +=1
+  end
+  print array
+  puts array
+end
+checksorted([1])
--- TDD_test.rb
+++ TDD_test.rb
@@ -2,15 +2,26 @@ require 'minitest/autorun'
 
 class RandomTest < Minitest::Test
 
-  def sort(array)
-    if array == [] || array = [1]
-      return array
+def sort(array)
+  i = 0
+  until i > array.length-2 do
+    
+    if array[i] > array[i+1] then
+    #if not sorted, must sort
+      print array  
+    else
+      break
     end
+    i +=1
   end
+  # print array
+  return array
+end
+
 
   def test_sort
     assert_equal([], sort([]))
-    assert_equal([1], sort([1])) 
+    assert_equal([1], sort([1]))
     assert_equal([1,2], sort([2,1]))
   end
 
